d31a9ac28542884135e0f3841a511f38cdc990f4
Hendy Tanata <htanata@gmail.com>
2017-03-27 20:28:46 -0700

    Fix ineffective usage of private.

--- lib/git_snip/branch.rb
+++ lib/git_snip/branch.rb
@@ -2,34 +2,36 @@ module GitSnip
   module Branch
     Row = Struct.new(:sha, :name, :date, :author, :message)
 
-    def self.row(branch)
-      Row.new.tap do |row|
-        row.sha = column(branch.gcommit.sha, 7)
-        row.name = column(branch.name, 12)
-        row.date = column(branch.gcommit.date.strftime('%F'), 10)
-        row.author = column(branch.gcommit.author.email.sub(/@.*/, ''), 8)
-        row.message = column(first_line(branch.gcommit.message), 39)
+    class << self
+      def row(branch)
+        Row.new.tap do |row|
+          row.sha = column(branch.gcommit.sha, 7)
+          row.name = column(branch.name, 12)
+          row.date = column(branch.gcommit.date.strftime('%F'), 10)
+          row.author = column(branch.gcommit.author.email.sub(/@.*/, ''), 8)
+          row.message = column(first_line(branch.gcommit.message), 39)
+        end
       end
-    end
 
-    def self.full_row(branch)
-      Row.new.tap do |row|
-        row.sha = branch.gcommit.sha
-        row.name = branch.name
-        row.date = branch.gcommit.date.iso8601
-        row.author = branch.gcommit.author.email
-        row.message = first_line(branch.gcommit.message)
+      def full_row(branch)
+        Row.new.tap do |row|
+          row.sha = branch.gcommit.sha
+          row.name = branch.name
+          row.date = branch.gcommit.date.iso8601
+          row.author = branch.gcommit.author.email
+          row.message = first_line(branch.gcommit.message)
+        end
       end
-    end
 
-    private
+      private
 
-    def self.column(string, width)
-      string[0, width].ljust(width)
-    end
+      def column(string, width)
+        string[0, width].ljust(width)
+      end
 
-    def self.first_line(string)
-      string.gsub(/[\r\n].*/, '')
+      def first_line(string)
+        string.gsub(/[\r\n].*/, '')
+      end
     end
   end
 end
